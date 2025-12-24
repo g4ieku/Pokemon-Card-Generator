2969db018c98149abd5ac290cb8506d66aff3dfb
g4ieku <121449657+g4ieku@users.noreply.github.com>
2025-12-24 14:59:04 +0100

    add fetchApi

---
+++ script.js
@@ -0,0 +1,55 @@
+const url = "https://pokeapi.co/api/v2/pokemon/";
+
+const pokemonCard = document.querySelector(".card");
+const pokemonImg = pokemonCard.querySelector("img");
+const pokemonName = pokemonCard.querySelector(".pokemon_name");
+const pokemonHP = pokemonCard.querySelector(".hp");
+const pokemonTypes = pokemonCard.querySelector(".types");
+const pokemonStats = pokemonCard.querySelectorAll(".stats h3");
+const btn = document.querySelector(".btn");
+
+const getPokemon = async (url, id) => {
+  try {
+    const link = `${url}${id}`;
+    console.log(link);
+    const response = await fetch(link);
+
+    if (!response.ok) throw new Error("Request Failed");
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const generatePokemon = async () => {
+  const id = Math.floor(Math.random() * 150) + 1;
+  const pokemonData = await getPokemon(url, id);
+  generateCard(pokemonData);
+};
+
+const generateCard = (data) => {
+  pokemonHP.lastChild.nodeValue = data.stats[0].base_stat;
+  pokemonName.textContent = data.name;
+  pokemonImg.src = data.sprites.other.dream_world.front_default;
+  pokemonStats[0].textContent = data.stats[1].base_stat;
+  pokemonStats[1].textContent = data.stats[2].base_stat;
+  pokemonStats[2].textContent = data.stats[5].base_stat;
+
+  pokemonTypes.innerHTML = "";
+
+  data.types.forEach((item) => {
+    const span = document.createElement("span");
+    span.appendChild(document.createTextNode(item.type.name));
+    pokemonTypes.appendChild(span);
+  });
+};
+
+const init = () => {
+  btn.addEventListener("click", generatePokemon);
+  document.addEventListener("DOMContentLoaded", generatePokemon);
+};
+
+init();
